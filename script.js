e0e052430caab73450e3e4b68e1e800d9621d5f8
keishaaquino2-netizen <keishaaquino2@gmail.com>
2025-10-05 19:49:46 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const msgEl = document.getElementById('message');
+const btn = document.getElementById('reveal');
+const heart = document.getElementById('heart');
+
+const message = [
+  "I like you",
+  "",
+  "To be honest, I don’t really know how to express what I truly feel.",
+  "You’re my sunshine  when I’m with you, everything else fades away.",
+  "When we talk, everything feels okay.",
+  "",
+  "I don’t want to sound corny when I’m in love, but this is my first time doing something like this, hehe.",
+  "Please take care of yourself.",
+  "",
+  "really, really like you, my bebi. 💜"
+].join('\n');
+
+// rest of your script.js stays the same below this line
